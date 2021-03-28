c835eb47848ba1be5dcf22a830281b2a148d0418
phoiv <phoebus.di@gmail.com>
2021-03-28 21:06:04 +0300

    changess

--- src/components/GameHeader/GameHeader.js
+++ src/components/GameHeader/GameHeader.js
@@ -3,10 +3,13 @@ import classes from './GameHeader.module.css'
 
 
 const gameHeader = (props) => {
+
+
+
     return (
         <div className={classes.gameHeader}>
             <div>{props.minesLeft}</div>
-            <div onClick={props.doubleClicked}> {props.children}</div>
+            <div onClick={props.clicked}> {props.children}</div>
             <div>{props.time}</div>
         </div>
     );
--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -2,21 +2,28 @@ import React from 'react';
 import classes from './Settings.module.css'
 
 
-const conf = {
-    begginer: [9, 9, 10],
-    intermediate: [16, 16, 40],
-    expert: [16, 30, 99],
-    custom: [null, null, null],
-}
+// const conf = {
+//     begginer: [9, 9, 10],
+//     intermediate: [16, 16, 40],
+//     expert: [16, 30, 99],
+//     custom: [null, null, null],
+// }
 
 const Settings = (props) => {
     return (
         <div className={classes.settings}>
+            <p onClick={props.clicked} className={classes.close}>X</p>
+            <div className={classes.labels}>
+                <p>SETTINGS</p>
+                <p>width</p>
+                <p>height</p>
+                <p>mines</p>
+            </div>
             <form onSubmit={props.submitted}>
                 <div className={classes.formrow} >
                     <div>
                         <input type="radio" id="beg" name="setting" value="beginner" />
-                        <label for="beg">beginner</label>
+                        <label for="beg"> Beginner</label>
                     </div>
                     <div>9</div>
                     <div>9</div>
@@ -26,7 +33,7 @@ const Settings = (props) => {
                 <div className={classes.formrow}>
                     <div >
                         <input type="radio" id="int" name="setting" value="intermediate" />
-                        <label for="int">intermediate</label>
+                        <label for="int"> Intermediate</label>
                     </div>
                     <div>16</div>
                     <div>16</div>
@@ -35,7 +42,7 @@ const Settings = (props) => {
                 <div className={classes.formrow}>
                     <div>
                         <input type="radio" id="exp" name="setting" value="expert" />
-                        <label for="exp">expert</label>
+                        <label for="exp"> Expert</label>
                     </div>
                     <div>16</div>
                     <div>30</div>
@@ -44,7 +51,7 @@ const Settings = (props) => {
                 <div className={classes.formrow}>
                     <div>
                         <input type="radio" id="cus" name="setting" value="custom" />
-                        <label for="cus">custom</label>
+                        <label for="cus"> custom</label>
                     </div>
                     <div><input className={classes.custominput} type="text" name="cW" /></div>
                     <div><input className={classes.custominput} type="text" name="cH" /></div>
@@ -57,7 +64,7 @@ const Settings = (props) => {
 
 
             </form>
-        </div >
+        </div>
     );
 }
 
